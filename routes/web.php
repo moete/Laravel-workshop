28b64d60306ca2e43db149c23c3005c0cea56605
moete <moetez.boubakri@esprit.tn>
2021-09-24 16:34:55 +0200

    integrate template

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/dashboard',function (){
+    return view('dashboard');
+});
+Route::get('/welcome',function () {
     return view('welcome');
 });
+
